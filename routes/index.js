02a6f05d934353e7443d83d91ed2b62cd10b1dfc
kvo92 <88671104+kvo92@users.noreply.github.com>
2022-09-12 15:32:44 -0400

    init commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "MakeReddit" });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+module.exports = router;
